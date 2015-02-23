c58376f52228affc7810e106d6fde8160dba72f1
otknoy <otknoy@gmail.com>
2015-02-23 09:53:13 +0900

    First commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var app = {};
+
+app.getGeolocation = function() {
+    var deferred = Promise.defer();
+
+    if ("geolocation" in navigator) {
+	navigator.geolocation.getCurrentPosition(function(position) {
+	    deferred.resolve(position.coords);
+	});
+    } else {
+	deferred.reject("Geolocation API is not available.");
+    }
+
+    return deferred.promise;
+};
